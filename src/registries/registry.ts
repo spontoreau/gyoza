397437ea141b755d9eb0258da58666ca1f431dd1
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2019-12-13 01:24:48 +0100

    :recycle: Change ctors array into a Map

--- src/registries/registry.ts
+++ src/registries/registry.ts
@@ -1,18 +1,19 @@
 import { RegistryType } from "./registryType";
 
 class Registry<T> {
-  private ctorDefinitions: Array<[Symbol, new (...args: unknown[]) => T]> = [];
+  private readonly ctors: Map<Symbol, new (...args: unknown[]) => T>;
 
-  constructor(public readonly type: RegistryType) {}
+  constructor(public readonly type: RegistryType) {
+    this.ctors = new Map();
+  }
 
   add(key: Symbol, handler: new (...args: unknown[]) => T) {
-    this.ctorDefinitions.push([key, handler]);
+    this.ctors.set(key, handler);
   }
 
   getInstance(key: Symbol): T {
-    const ctorDefinition = this.ctorDefinitions.find(h => h[0] === key) as [Symbol, new (...args: unknown[]) => T];
-    const ctor = ctorDefinition[1];
-      return new ctor();
+    const ctor = this.ctors.get(key) as new (...args: unknown[]) => T;
+    return new ctor();
   }
 }
 
