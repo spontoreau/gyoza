36d27c9c0aa4f3d7edbaccd41643a67abd4ace88
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2019-12-20 00:49:20 +0100

    :sparkles: Add first exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { QueryDispatcher } from "./core/queryDispatcher";
+export { QueryHandler } from "./decorators/queryHandler";
+export { QueryAlreadyUsedException } from "./exceptions/queryAlreadyUsedException";
+export { QueryUnassignedException } from "./exceptions/queryUnassignedException";
+export { UnknowQueryException } from "./exceptions/unknowQueryException";
+export { Dispatcher } from "./types/dispatcher";
+export { Handler } from "./types/handler";
